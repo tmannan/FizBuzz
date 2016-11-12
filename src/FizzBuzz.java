d017ee4f41e7569ddf7d06b8a858d687740dbf3a
Tinku Mannan <tmannan@msn.com>
2016-11-12 09:25:20 -0500

    FizzBuzz: declare count variable as part of for loop

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,9 +2,8 @@
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		int count;
 		// TODO Auto-generated method stub
-		for (count = 1;count <= 100;count++)
+		for (int count = 1;count <= 100;count++)
 		{
 			if ((count%3 == 0) && (count%5 == 0))
 				System.out.println("FizzBuzz: " + count);
